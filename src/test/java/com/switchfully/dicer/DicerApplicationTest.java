b2f1f5dd8072bf9ccb9aef87b2ceda5a1372c2c6
Adrianna Domanowska <ada-dom@hotmail.com>
2019-10-15 15:18:27 +0200

    maven codelab03

---
+++ src/test/java/com/switchfully/dicer/DicerApplicationTest.java
@@ -0,0 +1,15 @@
+package com.switchfully.dicer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DicerApplicationTest {
+
+    @Test
+    void justADummyTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
